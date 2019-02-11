b9aac4f10275487f509f14eb9893d393d9f20d49
whatWHAAT <wfdsfsdfwerwrfd@yandex.ru>
2019-02-11 09:30:00 +0300

    1102

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,7 @@
-public class MyFirstProgram {
+package ru.stqa.pft.sandbox;
 
+public class MyFirstProgram {
 	public static void main (String[] args) {
 System.out.println("Hello, world!");
 	}
-
 }
